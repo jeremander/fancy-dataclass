3ae8c90b96f59a549663f79d820c69eea60d2b62
Jeremy Silver <jeremys@nessiness.com>
2024-03-20 18:56:49 -0400

    build: migrate from poetry to hatch, add version number

--- fancy_dataclass/__init__.py
+++ fancy_dataclass/__init__.py
@@ -1,7 +1,9 @@
-# flake8: noqa
-
+# top-level class exports
 from .cli import ArgparseDataclass, CLIDataclass
 from .dict import DataclassMixin, DictDataclass
 from .json import JSONBaseDataclass, JSONDataclass, JSONSerializable
 from .sql import SQLDataclass
 from .subprocess import SubprocessDataclass
+
+
+__version__ = '0.1.0'
